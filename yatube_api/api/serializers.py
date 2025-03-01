a5fea69c92c19ce32a8ef0b87d464dfe04a84a6e
Иван Демин <wert227752@gmail.com>
2025-03-01 13:36:45 +0700

    PEP8.2

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -11,8 +11,8 @@ User = get_user_model()
 class Base64ImageField(serializers.ImageField):
 
     def to_internal_value(self, data):
-        if (isinstance(data, str) and 
-                data.startswith("data:image")):
+        if (isinstance(data, str)
+                and data.startswith("data:image")):
             format, imgstr = data.split(";base64,")
             ext = format.split("/")[-1]
             data = ContentFile(base64.b64decode(imgstr),
